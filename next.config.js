3f18cd20d728cf889d8961f39713ca2db3df1721
Teerut26 <teerutsr@gmail.com>
2024-11-03 22:16:59 +0700

    feat: add Dockerfile and .dockerignore; update next.config.js for standalone output

--- next.config.js
+++ next.config.js
@@ -6,9 +6,12 @@ import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
 const config = {
-    env: {
-        NEXT_PUBLIC_BASE_API: "https://general-badbuddy-be.tu4rl4.easypanel.host/api"
-    }
+  env: {
+    NEXT_PUBLIC_BASE_API:
+      "https://general-badbuddy-be.tu4rl4.easypanel.host/api",
+  },
+  output: "standalone",
+  swcMinify: true,
 };
 
 export default config;
